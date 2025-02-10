add9a5c943b81ca14763c5f65816656c0ff73ae3
preetinegi20 <negipreeti4245@gmail.com>
2025-02-10 11:40:39 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./", // ✅ Ensures assets load correctly in production
-  server: {
-    port: process.env.PORT || 5173, // ✅ Allows Railway to assign a port
-    host: "0.0.0.0", // ✅ Required for Railway to expose the server
+  built: {
+    outDir: "dist",
   },
 });
