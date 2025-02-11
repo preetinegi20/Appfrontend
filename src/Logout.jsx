e2ccc005114c55c505ac861914252451e991a8de
preetinegi20 <negipreeti4245@gmail.com>
2025-02-11 14:18:56 +0530

    soln

--- src/Logout.jsx
+++ src/Logout.jsx
@@ -4,10 +4,11 @@ import { useNavigate } from "react-router-dom";
 
 function Logout() {
   const navigate = useNavigate();
+  const apiUrl = import.meta.env.VITE_API_URL;
 
   const handleLogout = () => {
     axios
-      .post("http://localhost:2000/api/logout", {}, { withCredentials: true })
+      .post(`${apiUrl}/api/logout`, {}, { withCredentials: true })
       .then(() => {
         navigate("/login"); // Redirect to login after logout
       })
